3a33ccf6d51b362d59dd4fbe0b6b40ec9bfaa1a7
Holger R <holger.rosencrantz@taby.se>
2025-08-22 09:40:10 +0200

    Första lektionen

---
+++ hello.py
@@ -0,0 +1,10 @@
+print("Hej världen!")
+print("En till rad")    # Detta är en kommentar, den ignoreras av kompilatorn (körs ej)
+# en kommentar över en rad börjar med hashtag/brädhög
+"""
+Tre citattecken (eller apostroftecken) betyder att en kommentar över flera rader börjar.
+Allt som står efter det ignoreras tills det kommer ytterligare tre citattecken.
+"""
+x = 1       # skapar en "variabel" som heter x och har värdet 1
+y = 2       # dito y med värdet 2
+print(x+y)  # skriver ut 3
