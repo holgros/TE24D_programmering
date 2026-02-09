cd473df76c7617965332e536ffda02990e8ff1b7
Holger R <holger.rosencrantz@taby.se>
2026-02-09 10:35:53 +0100

    Mall för att lösa Kattis-problem och andra problem som handlar om input och output

---
+++ vt v07/script.js
@@ -0,0 +1,10 @@
+let textarea = document.getElementById("input");
+let runBtn = document.getElementById("run");
+let outputElement = document.getElementById("output");
+runBtn.addEventListener("click", function() {
+    let input = textarea.value;
+    let output;
+    // Här löser du problemet, tilldela output-variabeln ditt svar
+    //...
+    outputElement.innerHTML = output;
+});
